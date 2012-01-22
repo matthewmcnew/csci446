459842015e42e0bf77c9bd0a7981c4e3764681de
Matthew McNew <matthewmcnew@gmail.com>
2012-01-22 16:21:18 -0700

    Level 5 complete

--- project02/rubywarrior/matthew-beginner/player.rb
+++ project02/rubywarrior/matthew-beginner/player.rb
@@ -10,7 +10,11 @@ class Player
         @health = 0
       end
      else
+      if warrior.feel.captive? 
+        warrior.rescue!
+      else
         warrior.attack!
+      end
      end
   end
 end
