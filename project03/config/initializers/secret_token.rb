5960cc28def6e80e2e23c63c2cf2cdb2733e2fa8
Matthew McNew <matthewmcnew@gmail.com>
2012-02-01 23:29:42 -0700

    Adding Project 03

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = 'a716dd4b7e9e0d40442155eca5bad0cc212109811d4bab4cf69355224a215831136819f28cbd9e354e330ae0b84ae6496c455d50375e55b5e20214173cb393e6'
